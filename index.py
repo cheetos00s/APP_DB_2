1729b29a21d2ee69612542927ffb5182303461c1
cheetos00s <cardenasp456@gmail.com>
2023-06-03 22:29:34 -0500

    first commit

---
+++ index.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    info = ("xddd", "huevo", "hambre", "zanahoria", "gato")
+    return render_template('index.html', informacion=info)
+
+"""
+@app.route("/contacto")
+def contacto():
+    return "pagina de contacto"
+"""
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
