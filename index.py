76760611c9a7259eefa00e486af9ddebc626eee3
cheetos00s <cardenasp456@gmail.com>
2023-06-03 22:44:15 -0500

    mkia

--- index.py
+++ index.py
@@ -7,11 +7,11 @@ def home():
     info = ("xddd", "huevo", "hambre", "zanahoria", "gato")
     return render_template('index.html', informacion=info)
 
-"""
+
 @app.route("/contacto")
 def contacto():
     return "pagina de contacto"
-"""
+
 
 if __name__ == '__main__':
     app.run(debug=True)
