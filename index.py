965679487aa23bb4b948986da38596caae246ad2
Joseph_fps <jfsolanop@unbosque.edu.co>
2023-06-06 15:59:17 -0500

    Add the visualization of the tables with your queries

--- index.py
+++ index.py
@@ -47,8 +47,7 @@ def upload():
       mycursor.execute(sql,values)
       mydb.commit()
       
-    mycursor.close()
-    mydb.close()
+
   
     return redirect(url_for('home'))
 
@@ -68,44 +67,168 @@ def insert_data():
         # Confirma los cambios en la base de datos
         mydb.commit()
 
-        # Cierra la conexión
-        mycursor.close()
-        mydb.close()
+    
+
 
         return redirect(url_for('tables'))
 
     except mysql.connector.Error as error:
         
       return f'Error al ejecutar el procedimiento almacenado: {str(error)}'
-      
-@app.route("/query_aux", methods=['POST'])
-def query_aux():
-  # Crear una lista de objetos TableData
-  query_aux = "select * from tabla_aux"
-  mycursor.execute(query_aux)
-  result_aux = mycursor.fetchall()
-  
-  mycursor.close()
-  mydb.close
-  return render_template("tables.html", data=result_aux)
 
 
-@app.route("/query_tr", methods=['POST'])
 def query_tr():
-  # Crear una lista de objetos TableData
-  query_tr = "select * from tratamiento_registro"
-  mycursor.execute(query_tr)
-  result_tr = mycursor.fetchall()
-  
-  mycursor.close()
-  mydb.close
-  return render_template("tables.html", data=result_tr)
+    try:
+        # Crear una lista de objetos TableData
+        mycursor = mydb.cursor(dictionary=True)
+        query_tr = ("select * from tratamiento_registro")
+        mycursor.execute(query_tr)
+        result_tr = mycursor.fetchall()
 
-  
+        
+
+        return result_tr
+
+    except mysql.connector.Error as error:
+        return f'Error al ejecutar la consulta: {str(error)}'
+
+def query_donacion():
+    try:
+        # Crear una lista de objetos TableData
+        mycursor = mydb.cursor(dictionary=True)
+        query_don = ("select * from donacion")
+        mycursor.execute(query_don)
+        result_don = mycursor.fetchall()
+        
+       
+
+        return result_don
+
+    except mysql.connector.Error as error:
+        return f'Error al ejecutar la consulta: {str(error)}'
+
+ 
+
+def query_donante():
+    try:
+        # Crear una lista de objetos TableData
+        mycursor = mydb.cursor(dictionary=True)
+        query_dona = ("select * from donante")
+        mycursor.execute(query_dona)
+        result_dona = mycursor.fetchall()
+        
+
+        return result_dona
+
+    except mysql.connector.Error as error:
+        return f'Error al ejecutar la consulta: {str(error)}'
+
+def query_refugio():
+    try:
+        # Crear una lista de objetos TableData
+        mycursor = mydb.cursor(dictionary=True)
+        query_re = ("select * from refugio")
+        mycursor.execute(query_re)
+        result_re = mycursor.fetchall()
+        
+    
+
+        return result_re
+
+    except mysql.connector.Error as error:
+        return f'Error al ejecutar la consulta: {str(error)}' 
+
+def query_mascota():
+    try:
+        # Crear una lista de objetos TableData
+        mycursor = mydb.cursor(dictionary=True)
+        query_mas = ("select * from mascota")
+        mycursor.execute(query_mas)
+        result_mas = mycursor.fetchall()
+        
+        print(result_mas)
+        
+
+        return result_mas
+
+    except mysql.connector.Error as error:
+        return f'Error al ejecutar la consulta: {str(error)}' 
+
       
+def query_sede():
+    try:
+        # Crear una lista de objetos TableData
+        mycursor = mydb.cursor(dictionary=True)
+        query_se = ("select * from sede")
+        mycursor.execute(query_se)
+        result_se = mycursor.fetchall()
+        
+
+
+        return result_se
+
+    except mysql.connector.Error as error:
+        return f'Error al ejecutar la consulta: {str(error)}' 
+ 
+def query_tratamiento():
+    try:
+        # Crear una lista de objetos TableData
+        mycursor = mydb.cursor(dictionary=True)
+        query_tra = ("select * from tratamiento")
+        mycursor.execute(query_tra)
+        result_tra = mycursor.fetchall()
+
+    
+
+        return result_tra
+
+    except mysql.connector.Error as error:
+        return f'Error al ejecutar la consulta: {str(error)}' 
+      
+def query_sitio_rescate():
+    try:
+        # Crear una lista de objetos TableData
+        mycursor = mydb.cursor(dictionary=True)
+        query_res = ("select * from sitio_rescate")
+        mycursor.execute(query_res)
+        result_res = mycursor.fetchall()
+
+        
+
+        return result_res
+
+    except mysql.connector.Error as error:
+        return f'Error al ejecutar la consulta: {str(error)}' 
+
+def query_persona():
+    try:
+        # Crear una lista de objetos TableData
+        mycursor = mydb.cursor(dictionary=True)
+        query_per = ("select * from persona")
+        mycursor.execute(query_per)
+        result_per = mycursor.fetchall()
+
+        
+
+        return result_per
+
+    except mysql.connector.Error as error:
+        return f'Error al ejecutar la consulta: {str(error)}' 
+      
+           
 @app.route("/tables")
 def tables():
-    return render_template('tables.html')
+    result_tr = query_tr()
+    result_donacion = query_donacion()
+    result_donante = query_donante()
+    result_refugio = query_refugio()
+    result_mascota = query_mascota()
+    result_sede = query_sede()
+    result_tratamiento = query_tratamiento()
+    result_persona = query_persona()
+    result_sitio_rescate = query_sitio_rescate()
+    print(result_tratamiento)
+    return render_template('tables.html', datatr = result_tr, dataTratamiento = result_tratamiento, dataDonacion = result_donacion, dataSede = result_sede, dataRefugio = result_refugio, dataDonante = result_donante, dataMascota = result_mascota, dataPersona = result_persona, dataSitio = result_sitio_rescate)
 
 
 if __name__ == '__main__':
